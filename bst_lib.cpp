43708c600b3d6ef1f3c03065fda9658fefcddf77
Cimpoies Samuel <68305744+cimpoiessamuel@users.noreply.github.com>
2024-11-26 15:40:32 +0100

    searchR, isBst, height definition added

--- bst_lib.cpp
+++ bst_lib.cpp
@@ -19,6 +19,48 @@ Node* Node::insertR(int k){
     return this;
 }
 
+int Node::height(){
+    if(this == NULL) return 0;
+
+    int r = rchild->height();
+    int l = lchild->height();
+
+    return max(r,l) + 1;
+}
+
+Node* Node::searchR(int k){
+    if(this == NULL) return NULL;
+
+    if(k == data) return this;
+
+    if(k > data) return rchild->searchR(k);
+    if(k < data) return lchild->searchR(k);
+
+    return NULL;
+}
+
+bool Node::isBst(){
+    if(this == NULL) return true;
+
+    if(rchild != NULL){
+        if(rchild->data > data){
+            rchild->isBst();
+        }else{
+            return false;
+        }
+    }
+
+    if(lchild != NULL){
+        if(lchild->data < data){
+            lchild->isBst();
+        }else{
+            return false;
+        }
+    }
+
+    return true;
+}
+
 void Node::inOrder(){
     if(this == NULL) return;
 
